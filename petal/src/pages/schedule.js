28ef7fd979ad31d5169c0a2a8a71db8ce8a9bfce
KCNC36 <khatecastelo36@gmail.com>
2024-11-20 15:31:39 +0000

    calendar inserted

--- petal/src/pages/schedule.js
+++ petal/src/pages/schedule.js
@@ -1,12 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
+import Calendar from "react-calendar";
+import "react-calendar/dist/Calendar.css";
+
 
 const Schedule = () => {
-    return (
-        <>
-            <h1>focus on this page this week</h1>
-            <h3>input calendar and date will pop up on the right box w/ events of the day</h3>
-        </>
-    );
+
+    const [date, changeDate] = useState(new Date());
+
+    function changeValue(val) {
+       changeDate(val);
+    }
+
+  return (
+      <div>
+         <h3>
+            {" "} Using the <i> react-calender </i> library to create calender in React JS{" "}
+         </h3>
+         <Calendar onChange = {changeValue} value = {date} />
+         <p>The selected date is - {date.toLocaleDateString()}</p>
+      </div>
+   );
 };
 
 export default Schedule;
