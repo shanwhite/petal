0b1b7eb54987f52c6ad1cae088f774ef54530b9f
KCNC36 <khatecastelo36@gmail.com>
2024-12-03 14:14:40 +0000

    began designing pets and added a design folder for all CSS files

--- petal/src/pages/pets.js
+++ petal/src/pages/pets.js
@@ -1,11 +1,19 @@
 import React from "react";
+import '../design/pets.css'; 
 
 function Pets() {
     return (
-        <div className="pets">
-            <h1>set up pet profiles here</h1>
+        //The parent elements
+        <div className="pets"> <h1>set up pet profiles here</h1>
+            <div className="test"> TESTING </div>
+
+
+
+
+
         </div>
+
     );
 };
 
-export default Pets;
+export default Pets; 
