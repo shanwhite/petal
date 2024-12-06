d8437833e3ce0a55586e641777fae07e179e71a7
KCNC36 <khatecastelo36@gmail.com>
2024-12-06 12:00:11 +0000

    testing changes

--- petal/src/pages/pets.js
+++ petal/src/pages/pets.js
@@ -1,12 +1,37 @@
 import React from "react";
-import '../design/pets.css';
+import '../design/pets.css'; 
+import petImage from '../assets/pet.png';
 
 function Pets() {
     return (
+        //The parent element
         <div className="pets">
-            <div class="container"> TESTING</div>
+            <div className="container">
+            <div className="test"> TESTING </div>
+            <div className="profile-container"> 
+
+
+                 <div class="image-container"> 
+                    <img src={petImage} alt="Pet" className="image-content"/>
+                 </div>
+
+                <div className="text">
+                    <h2> TESTING 2</h2>
+                    <p>This is to see the structure in pets </p>
+                </div>
+
+            </div>
+            </div>
+
+
+
+
+
         </div>
+
     );
 };
 
-export default Pets;
+export default Pets; 
+
+
