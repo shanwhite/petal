7b2303bb1a28e8ba20bb058c241583c1fd808e79
Shan <127458584+shanwhite@users.noreply.github.com>
2025-01-03 04:11:32 +0000

    neatened index.css & commented more for index.css & schedule.js

--- petal/src/pages/schedule.js
+++ petal/src/pages/schedule.js
@@ -13,15 +13,20 @@ function Schedule() {
       <div className="schedule">
          <div id="calendar">
             <Calendar key={date.toString()} onChange={changeValue} value={date} />
-            {/* clicking reset button will bring the user back to the current page */}
+            {/* clicking reset button will bring the user back to today's date */}
             <button id="reset-btn" onClick={() => changeDate(new Date())}>Reset</button>
          </div>
          <div id="event-box">
             <p id="event-date">{date.toLocaleDateString()}</p>
+            <br />
             <table>
                <tbody>
                   {petData
+                  // when the user clicks on a specific date of the calendar,
+                  // if the selected date matches the careDate from the JSON file (../data/pets.js),
+                  // display the name, breed, and to-do list in the event box
                   .filter((pet) => pet.careDate === date.toLocaleDateString())
+                  // render the name, breed and toDoList properties from pets.js
                   .map((pet, index) => (
                      <tr key={index}>
                         <td id="schedule-details">
@@ -31,7 +36,7 @@ function Schedule() {
                            <br />
                            <div><span style={{fontWeight:"bold"}}>To-Do List: </span>
                               {pet.toDoList
-                              // render all properties of toDoList from pets.js
+                              // render all properties of toDoList from pets.js as a list
                               .map((toDo, i) => (
                                 <li key={i} id="schedule-list">{toDo}</li>
                               ))}
