4706f5ee8a54a70e45c8424ea8c58a2617b95336
Shan <127458584+shanwhite@users.noreply.github.com>
2025-01-03 04:57:32 +0000

    Update index.js

--- petal/src/pages/index.js
+++ petal/src/pages/index.js
@@ -27,24 +27,24 @@ function Home() {
 
     return (
         <div className="home">
-                <section className="banner">
-                    <div className="logo-text-container">
-                        <div className="logoContainer">
-                            {/* FUNCTIONALITY: Conditional Rendering */}
-                            {userDetails ? (<div className="welcome"><h1>𑁍ࠬܓWelcome {userDetails.username}</h1></div> ):(<p>Loading...</p>)}
-                            <img src={Logo} alt="logo" className="logo"/>
-                        </div>
-                        <div className="rectangle">
-                            <div className="innerRectangle">
-                            <h1>What are we about?</h1>
-                            <p><u>Petal</u> is a Pet Care Assistant app which keeps track of all pet-related responsibilities, including feeding, grooming, vet appointments, and walks.<br /><br />
-                            Its key features include a schedule for feeding and exercises, reminders for medication, milestone tracking for vaccinations, and pet-related tips.</p><br/>
-                            <div className="cat">⊹₊⋆ฅ^•ﻌ•^ฅ₊⋆⊹</div>
-                            <p>Have fun exploring our application!</p>
-                            </div>
+            <section className="banner">
+                <div className="logo-text-container">
+                    <div className="logoContainer">
+                        {/* FUNCTIONALITY: Conditional Rendering */}
+                        {userDetails ? (<div className="welcome"><h1>𑁍ࠬܓWelcome {userDetails.username}</h1></div> ):(<p>Loading...</p>)}
+                        <img src={Logo} alt="logo" className="logo"/>
+                    </div>
+                    <div className="rectangle">
+                        <div className="innerRectangle">
+                        <h1>What are we about?</h1>
+                        <p><u>Petal</u> is a Pet Care Assistant app which keeps track of all pet-related responsibilities, including feeding, grooming, vet appointments, and walks.<br /><br />
+                        Its key features include a schedule for feeding and exercises, reminders for medication, milestone tracking for vaccinations, and pet-related tips.</p><br/>
+                        <div className="cat">⊹₊⋆ฅ^•ﻌ•^ฅ₊⋆⊹</div>
+                        <p>Have fun exploring our application!</p>
                         </div>
                     </div>
-                </section>
+                </div>
+            </section>
             <section className="banner2">
                 <div className="square"><h1>How it works</h1></div>
                 <div className="square2"><h1>My Pets</h1>
